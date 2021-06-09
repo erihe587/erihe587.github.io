ce83cd5318cbfa68042fa2d5857a2554f427d60e
Erik Norrestam Held <trapberry@localhost.localdomain>
2021-06-09 15:46:08 +0200

    All the things

--- flakypy_html.js
+++ flakypy_html.js
@@ -1,3 +1,37 @@
-selectRun = function(runNumber) {
-    
+toggleContent = function(elem) {
+    elem.classList.toggle("active")
+    var content = elem.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+selectDivRun = function() {
+    var sel = document.getElementById("selectDivRun");
+    var runNumber = sel.value;
+
+    // Hide all other runs
+    var allRuns = document.getElementsByClassName("divergence")
+    for (let run of allRuns) {
+        run.style.display = "none"
+    }
+
+    // Show selected run
+    document.getElementById(`div-${runNumber}`).style.display = "inline-flex";
+}
+
+selectAnomRun = function() {
+    var sel = document.getElementById("selectAnomRun");
+    var runNumber = sel.value;
+
+    // Hide all other runs
+    var allRuns = document.getElementsByClassName("anomalies")
+    for (let run of allRuns) {
+        run.style.display = "none"
+    }
+
+    // Show selected run
+    document.getElementById(`anom-${runNumber}`).style.display = "inline-flex";
 }
